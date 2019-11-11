869840e76ec831aa665425bd47c10bc4ef986d98
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-11 21:05:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const returnLastTwoDrivers = function(drivers) {
 const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ];
 
 const createFareMultiplier = function(multiplyValue) {
-  return function(value) 
+  return function(value) {
+    return 
+  }
 };
 
 const fareDoubler;
